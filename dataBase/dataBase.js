1a21e7afc14316c4f6a78e4bc510f5e3c7581c7d
ghanama ahmed <54191980+GHanamaAhmed@users.noreply.github.com>
2023-06-21 23:32:39 +0100

    commit

--- dataBase/dataBase.js
+++ dataBase/dataBase.js
@@ -1,60 +1,71 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const ModuleAboutMe = mongoose.model("aboutMe", new mongoose.Schema({
+const ModuleAboutMe = mongoose.model(
+  "aboutMe",
+  new mongoose.Schema({
     text: {
-        type: String,
-
-    }
-}))
-const ModuleWorkExperience = mongoose.model("workExperience", new mongoose.Schema({
+      type: String,
+    },
+  })
+);
+const ModuleWorkExperience = mongoose.model(
+  "workExperience",
+  new mongoose.Schema({
     job: {
-        type: String,
-
+      type: String,
     },
     type: {
-        type: String,
+      type: String,
     },
     company: {
-        type: String,
-
+      type: String,
     },
     location: {
-        type: String,
-
+      type: String,
     },
     datebegin: {
-        type: String,
-
+      type: String,
     },
     dateEnd: {
-        type: String,
-
-    }
-}))
-const ModuleEducation = mongoose.model("education", new mongoose.Schema({
+      type: String,
+    },
+  })
+);
+const ModuleEducation = mongoose.model(
+  "education",
+  new mongoose.Schema({
     specialization: {
-        type: String,
+      type: String,
     },
     type: {
-        type: String,
+      type: String,
     },
     school: {
-        type: String,
+      type: String,
     },
     location: {
-        type: String,
+      type: String,
     },
     datebegin: {
-        type: String,
+      type: String,
     },
     dateEnd: {
-        type: String,
-    }
-}))
-const TechStack=mongoose.model("tech",new mongoose.Schema({
-    img:{
-        type:String,
-        required:true
-    }
-}))
-module.exports = { ModuleAboutMe, ModuleWorkExperience, ModuleEducation ,TechStack}
+      type: String,
+    },
+  })
+);
+const TechStack = mongoose.model(
+  "tech",
+  new mongoose.Schema({
+    img: {
+      type: String,
+      required: true,
+    },
+  })
+);
+module.exports = {
+  ModuleAboutMe,
+  ModuleWorkExperience,
+  ModuleEducation,
+  TechStack,
+};
